e4c77e538d069f37bd11b3c8ff6d0df6e1fdc1e5
avinanick <avinanick@gmail.com>
2020-07-25 20:19:05 -0700

    Created a variable main_body in component for ease of access to the parent body.

--- Scripts/Components/Component.gd
+++ Scripts/Components/Component.gd
@@ -1,13 +1,12 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var main_body
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	main_body = get_parent()
 	pass # Replace with function body.
 
 
