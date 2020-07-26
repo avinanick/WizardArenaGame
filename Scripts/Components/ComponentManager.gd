ad3ac20ab841b28e575197c1afe009476f66c14b
avinanick <avinanick@gmail.com>
2020-07-25 19:03:42 -0700

    Creating basic parts for how I will be setting up this game. Wall, wizard, and component so far.

---
+++ Scripts/Components/ComponentManager.gd
@@ -0,0 +1,22 @@
+extends KinematicBody
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func propagate_message(var message: String):
+	for child in self.get_children():
+		if child.is_in_group("Components"):
+			child.receive_message(message)
+	pass
