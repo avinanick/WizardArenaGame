0f9ad9c645483aaf92fa38697a9bc03d9868f96c
Nick Avina <avinanick@gmail.com>
2020-07-26 20:46:06 -0700

    Implimented the function in the move component that responds to adding or starting a movement direction.

--- Scripts/Components/Component.gd
+++ Scripts/Components/Component.gd
@@ -7,7 +7,6 @@ var main_body
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	main_body = get_parent()
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Scripts/Components/MoveComponent.gd
+++ Scripts/Components/MoveComponent.gd
@@ -5,21 +5,36 @@ export var move_speed: float = 1.0
 export var min_move_time: float = 0 
 
 var move_direction: Vector2 = Vector2(0,0)
+var current_directions: Dictionary = {"Up":false,
+									"Down":false,
+									"Left":false,
+									"Right":false}
 
 signal end_move
 signal start_move
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_process(false)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
 
 func add_movement(var direction):
-	pass
+	set_process(true)
+	current_directions[direction] = true
+	move_direction = Vector2(0,0)
+	if current_directions["Up"]:
+		move_direction.y += 1
+	if current_directions["Down"]:
+		move_direction.y -= 1
+	if current_directions["Left"]:
+		move_direction.x -= 1
+	if current_directions["Right"]:
+		move_direction.x += 1
+	move_direction = move_direction.normalized()
 
 func receive_message(var message: Dictionary):
 	if message.has("StartMove"):
