f122f3319b4b95030caad5f891c2ca13ca5d5496
avinanick <avinanick@gmail.com>
2020-07-26 19:29:23 -0700

    Started work on the health component, but I just realized I'd rather start with movement and controls.

---
+++ Scripts/Components/HealthComponent.gd
@@ -0,0 +1,20 @@
+extends "res://Scripts/Components/Component.gd"
+
+
+export var health_points: int = 100
+
+signal taking_damage
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func receive_message(var message: Dictionary):
+	if message.has("Damage"):
+		emit_signal("taking_damage", message["Damage"])
