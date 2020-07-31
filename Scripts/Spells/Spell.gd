bdbb5fab13f3215f594c897ea010510a08b8aa63
avinanick <avinanick@gmail.com>
2020-07-30 22:13:08 -0700

    Adding values to the spell script to allow it to be customized for different spells.

--- Scripts/Spells/Spell.gd
+++ Scripts/Spells/Spell.gd
@@ -1,6 +1,12 @@
 extends Spatial
 
 
+export var spell_range: float = 2.0
+export var spell_effect: PackedScene
+export var is_targeted_spell: bool = true
+export var spell_name: String = ""
+export var spell_cooldown: float = 2.0
+
 var camera: Camera
 var targeting_spell: bool = false
 var target: Vector2 = Vector2(0,0)
@@ -29,7 +35,17 @@ func physics_process(_delta):
 #	pass
 
 func activate_spell():
-	pass
+	if is_targeted_spell:
+		print("This should drop a new spell effect at the target location, or as close as possible")
+		var spawn_location: Vector3 = Vector3(self.global_transform.origin.x, target.y, target.x)
+		if not spell_effect:
+			print("Error: No spell effect assigned to spell")
+			return
+		var new_spell_effect = spell_effect.instance()
+		new_spell_effect.global_transform = spawn_location
+		
+	else:
+		print("This should create the spell effect and launch it in the target direction")
 
 func target_spell():
 	targeting_spell = true
