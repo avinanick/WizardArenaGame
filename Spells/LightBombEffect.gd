a1317e7e0644141ebe4703b6d095b61f2aed67b6
avinanick <avinanick@gmail.com>
2020-08-06 18:27:34 -0700

    Started creating the methods that will be used in the light bomb effect script.

--- Spells/LightBombEffect.gd
+++ Spells/LightBombEffect.gd
@@ -1,9 +1,7 @@
 extends "res://Spells/SpellEffect.gd"
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var detonating: bool = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,3 +12,17 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func activate_effect():
+	$AnimationPlayer.play("detonate")
+
+# Called when an explostion animation finishes, at which point this effect should be deleted
+func finish_detonation():
+	self.queue_free()
+
+# Called when a body enters the area, should check if the light bomb is detonating, if so, damage
+# the body.
+func damage_body(var body):
+	if detonating and body.has_method("propagate_message"):
+		pass
+	pass
