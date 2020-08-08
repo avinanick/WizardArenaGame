aa75f000a985b5bc204e9fe950d7eb059a785a00
avinanick <avinanick@gmail.com>
2020-08-08 15:51:54 -0700

    Added a function to handle the wizard taking damage and connected it to the health component damage signal.

--- Components/HealthComponent.gd
+++ Components/HealthComponent.gd
@@ -8,12 +8,19 @@ signal taking_damage
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	self.connect("taking_damage", self, "handle_damage")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func handle_damage(var amount: int):
+	health_points -= amount
+	print("Remaining health: ", health_points)
+	if health_points <= 0:
+		print("Out of health!")
 
 func receive_message(var message: Dictionary):
 	if message.has("Damage"):
