a8672d1a79e48311d767caff2ce9d9251d5ff4ec
avinanick <avinanick@gmail.com>
2020-08-12 16:37:16 -0700

    Added a caster variable to the spell effect so for those that need to do something specifically to the wizard that casts it.

--- Spells/SpellEffect.gd
+++ Spells/SpellEffect.gd
@@ -6,6 +6,7 @@ export var effect_delay: float = 0
 export var effect_knockback: float = 0
 export var effect_speed: float = 0
 var effect_direction: Vector2 = Vector2(0,0) setget set_direction, get_direction
+var spell_caster
 
 
 # Called when the node enters the scene tree for the first time.
@@ -26,7 +27,8 @@ func get_direction() -> Vector2:
 func set_direction(var direction: Vector2):
 	effect_direction = direction
 
-func start_spell_effect():
+func start_spell_effect(var caster):
+	spell_caster = caster
 	print("Spell location at ", self.global_transform.origin)
 	if effect_delay > 0:
 		$Timer.wait_time = effect_delay
