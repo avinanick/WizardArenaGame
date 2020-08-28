0fef1ee699735d92d351e4b62ccb97d40fdcde2a
Nick Avina <avinanick@gmail.com>
2020-08-28 16:49:09 -0700

    Wrote out a first pass on choosing the main character for the battle manager.

--- GameManagement/BattleManager.gd
+++ GameManagement/BattleManager.gd
@@ -8,9 +8,18 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	randomize()
+	# For now this only deals with single player games, later this will need
+	# to be amended to handle multiplayer
+	choose_main_player()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func choose_main_player():
+	var arena_wizards = get_tree().get_nodes_in_group("Wizards")
+	var chosen_wizard = arena_wizards[randi() % arena_wizards.size()]
+	print("Set the chosen wizard as the main")
+	chosen_wizard.propagate_message({"MakeMain":true})
